55357837e20e069d6461f3c376599875dc628b8d
Chris Zheng <zcaudate@users.noreply.github.com>
2026-08-03 23:12:47 +1000

    feat: introduce the Greenways wayfinder sigil

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const pattern = [
-  "0011100",
-  "0100010",
-  "1000001",
-  "1001111",
-  "1000001",
-  "0100010",
-  "0011100",
-].join("");
-document.querySelector("[data-mark]").replaceChildren(
-  ...[...pattern].map((value) => {
-    const tile = document.createElement("i");
-    tile.dataset.on = value === "1" ? "true" : "false";
-    return tile;
-  }),
-);
 const button = document.querySelector("[data-theme-button]");
 const labels = { auto: "Auto", light: "Light", dark: "Dark" };
 const sync = () =>
